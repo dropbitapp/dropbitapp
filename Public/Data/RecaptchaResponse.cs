81aaf9bb22743a6e494bd5e22918595ea4f4d050
Stanislav Krasnyi <29313520+krasnyis@users.noreply.github.com>
2017-10-21 20:05:45 -0700

    Added public facing Web API project to the solution.

---
+++ Public/Data/RecaptchaResponse.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Public.Data
+{
+    public class RecaptchaResponse
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("error-codes")]
+        public IEnumerable<string> ErrorCodes { get; set; }
+
+        [JsonProperty("challenge_ts")]
+        public DateTime ChallengeTs { get; set; }
+
+        [JsonProperty("hostname")]
+        public string Hostname { get; set; }
+    }
+}
---
+++ Public/Models/ContactDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Public.Models
+{
+    public class ContactDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Response { get; set; }
+    }
+}
