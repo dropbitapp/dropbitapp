24725506c29bbef6a9ebc5eb54260f41d744daa6
Grisha Lyukshin <grigoriy.lyukshin@hotmail.com>
2017-10-31 20:02:04 -0700

    dev to prod push diff files

---
+++ WebApp/Migrations/201711010247251_fillTest.cs
@@ -0,0 +1,24 @@
+namespace WebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fillTest : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.FillTest", "AlcoholContent", c => c.Single(nullable: false));
+            AddColumn("dbo.FillTest", "FillVariation", c => c.Single(nullable: false));
+            AddColumn("dbo.FillTest", "CorrectiveAction", c => c.String());
+            DropColumn("dbo.FillTest", "ProofGallons");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.FillTest", "ProofGallons", c => c.Single(nullable: false));
+            DropColumn("dbo.FillTest", "CorrectiveAction");
+            DropColumn("dbo.FillTest", "FillVariation");
+            DropColumn("dbo.FillTest", "AlcoholContent");
+        }
+    }
+}
