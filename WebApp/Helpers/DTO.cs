701d43f22cc8a47197330d533fcc35bfb40785ca
Grigoriy Lyukshin <grigoriy.lyukshin@hotmail.com>
2017-12-25 19:25:29 +0000

    Merged PR 139: adding new dto file and deleting some git backup files that shouldn't have been commited
    adding new dto file and deleting some backup files that slipped in one of my earlier commits
    
    Related work items: #1721

---
+++ WebApp/Helpers/DTO.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApp.Helpers
+{
+    /// Objects used for transporting data back and forth back-end and front-end
+
+    /// <summary>
+    /// Used in Reporting workflows
+    /// </summary>
+    public class ReportDto
+    {
+        public int IdentifierID { get; set; }
+        public int PartID { get; set; }
+        public int RowID { get; set; }
+        public int ColumnID { get; set; }
+        public float Value { get; set; }
+        public string Custom { get; set; }
+    }
+}
