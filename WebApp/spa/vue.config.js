79cec0e10d54457f5f200c81929c85d8d2a7e2b6
Stanislav Krasnyi <29313520+krasnyis@users.noreply.github.com>
2018-12-10 20:12:53 -0800

    Updating proxy settings

--- WebApp/spa/vue.config.js
+++ WebApp/spa/vue.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production' ? '/spa/dist/' : '/',
   devServer: {
-    proxy: {
-      '*': {
-        target: 'https://localhost:44363',
-        changeOrigin: true,
-      },
-    },
+    proxy: 'http://localhost:51299',
   },
 };
