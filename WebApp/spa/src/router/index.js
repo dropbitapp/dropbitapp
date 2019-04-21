5ae8bbe96beef2bf2fc2fd739a47d54a77a3b3d2
Stanislav Krasnyi <29313520+krasnyis@users.noreply.github.com>
2019-04-21 06:42:34 +0000

    Merged PR 317: Gutting unused code from prototype phase
    Related work items: #2003

--- WebApp/spa/src/router/index.js
+++ WebApp/spa/src/router/index.js
@@ -22,16 +22,6 @@ const FermentedView = () => import('../views/purchase/FermentedView.vue');
 const DistilledView = () => import('../views/purchase/DistilledView.vue');
 const SupplyView = () => import('../views/purchase/SupplyView.vue');
 const AdditiveView = () => import('../views/purchase/AdditiveView.vue');
-const FermentableDetail = () => import('../views/purchase/FermentableDetail.vue');
-const FermentedDetail = () => import('../views/purchase/FermentedDetail.vue');
-const DistilledDetail = () => import('../views/purchase/DistilledDetail.vue');
-const SupplyDetail = () => import('../views/purchase/SupplyDetail.vue');
-const AdditiveDetail = () => import('../views/purchase/AdditiveDetail.vue');
-const FermentableEdit = () => import('../views/purchase/FermentableEdit.vue');
-const FermentedEdit = () => import('../views/purchase/FermentedEdit.vue');
-const DistilledEdit = () => import('../views/purchase/DistilledEdit.vue');
-const SupplyEdit = () => import('../views/purchase/SupplyEdit.vue');
-const AdditiveEdit = () => import('../views/purchase/AdditiveEdit.vue');
 
 // lazy load production views
 const Production = () => import('../views/production/Production.vue');
@@ -41,14 +31,6 @@ const FermentationView = () => import('../views/production/FermentationView.vue'
 const DistillationView = () => import('../views/production/DistillationView.vue');
 const BlendingView = () => import('../views/production/BlendingView.vue');
 const BottlingView = () => import('../views/production/BottlingView.vue');
-const FermentationDetail = () => import('../views/production/FermentationDetail.vue');
-const DistillationDetail = () => import('../views/production/DistillationDetail.vue');
-const BlendingDetail = () => import('../views/production/BlendingDetail.vue');
-const BottlingDetail = () => import('../views/production/BottlingDetail.vue');
-const FermentationEdit = () => import('../views/production/FermentationEdit.vue');
-const DistillationEdit = () => import('../views/production/DistillationEdit.vue');
-const BlendingEdit = () => import('../views/production/BlendingEdit.vue');
-const BottlingEdit = () => import('../views/production/BottlingEdit.vue');
 
 // lazy load dictionary views
 const Dictionary = () => import('../views/dictionary/Dictionary.vue');
@@ -58,14 +40,6 @@ const SpiritView = () => import('../views/dictionary/SpiritView.vue');
 const VendorView = () => import('../views/dictionary/VendorView.vue');
 const StorageView = () => import('../views/dictionary/StorageView.vue');
 const MaterialView = () => import('../views/dictionary/MaterialView.vue');
-const SpiritDetail = () => import('../views/dictionary/SpiritDetail.vue');
-const VendorDetail = () => import('../views/dictionary/VendorDetail.vue');
-const StorageDetail = () => import('../views/dictionary/StorageDetail.vue');
-const MaterialDetail = () => import('../views/dictionary/MaterialDetail.vue');
-const SpiritEdit = () => import('../views/dictionary/SpiritEdit.vue');
-const VendorEdit = () => import('../views/dictionary/VendorEdit.vue');
-const StorageEdit = () => import('../views/dictionary/StorageEdit.vue');
-const MaterialEdit = () => import('../views/dictionary/MaterialEdit.vue');
 
 // lazy load reporting views
 const StorageReport = () => import('../views/reporting/StorageReport.vue');
@@ -129,56 +103,6 @@ export default new Router({
       path: 'add',
       component: AddPurchaseView,
     },
-    {
-      path: 'fermentable/detail/:id',
-      component: FermentableDetail,
-      props: true,
-    },
-    {
-      path: 'fermented/detail/:id',
-      component: FermentedDetail,
-      props: true,
-    },
-    {
-      path: 'distilled/detail/:id',
-      component: DistilledDetail,
-      props: true,
-    },
-    {
-      path: 'supply/detail/:id',
-      component: SupplyDetail,
-      props: true,
-    },
-    {
-      path: 'additive/detail/:id',
-      component: AdditiveDetail,
-      props: true,
-    },
-    {
-      path: 'fermentable/edit/:id',
-      component: FermentableEdit,
-      props: true,
-    },
-    {
-      path: 'fermented/edit/:id',
-      component: FermentedEdit,
-      props: true,
-    },
-    {
-      path: 'distilled/edit/:id',
-      component: DistilledEdit,
-      props: true,
-    },
-    {
-      path: 'supply/edit/:id',
-      component: SupplyEdit,
-      props: true,
-    },
-    {
-      path: 'additive/edit/:id',
-      component: AdditiveEdit,
-      props: true,
-    },
     ],
   },
   {
@@ -209,46 +133,6 @@ export default new Router({
       path: 'add',
       component: AddProductionView,
     },
-    {
-      path: 'fermentation/detail/:id',
-      component: FermentationDetail,
-      props: true,
-    },
-    {
-      path: 'distillation/detail/:id',
-      component: DistillationDetail,
-      props: true,
-    },
-    {
-      path: 'blending/detail/:id',
-      component: BlendingDetail,
-      props: true,
-    },
-    {
-      path: 'bottling/detail/:id',
-      component: BottlingDetail,
-      props: true,
-    },
-    {
-      path: 'fermentation/edit/:id',
-      component: FermentationEdit,
-      props: true,
-    },
-    {
-      path: 'distillation/edit/:id',
-      component: DistillationEdit,
-      props: true,
-    },
-    {
-      path: 'blending/edit/:id',
-      component: BlendingEdit,
-      props: true,
-    },
-    {
-      path: 'bottling/edit/:id',
-      component: BottlingEdit,
-      props: true,
-    },
     ],
   },
   {
@@ -278,46 +162,6 @@ export default new Router({
     {
       path: 'add',
       component: AddDictionaryView,
-    },
-    {
-      path: 'spirit/detail/:id',
-      component: SpiritDetail,
-      props: true,
-    },
-    {
-      path: 'vendor/detail/:id',
-      component: VendorDetail,
-      props: true,
-    },
-    {
-      path: 'storage/detail/:id',
-      component: StorageDetail,
-      props: true,
-    },
-    {
-      path: 'material/detail/:id',
-      component: MaterialDetail,
-      props: true,
-    },
-    {
-      path: 'spirit/edit/:id',
-      component: SpiritEdit,
-      props: true,
-    },
-    {
-      path: 'vendor/edit/:id',
-      component: VendorEdit,
-      props: true,
-    },
-    {
-      path: 'storage/edit/:id',
-      component: StorageEdit,
-      props: true,
-    },
-    {
-      path: 'material/edit/:id',
-      component: MaterialEdit,
-      props: true,
     },
     ],
   },
