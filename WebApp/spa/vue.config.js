2c8028938ae975b9bf85b86eab906ffa066059da
Stan Krasnyi <29313520+krasnyis@users.noreply.github.com>
2020-06-10 14:59:10 -0700

    Updated Vue dependencies

--- WebApp/spa/vue.config.js
+++ WebApp/spa/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/spa/dist/' : '/',
   devServer: {
-    proxy: 'http://localhost:51299',
+    proxy: 'http://localhost:44363',
   },
 };
